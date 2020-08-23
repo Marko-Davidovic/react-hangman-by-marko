b26a2510483fa504cb481aae18e44088a9c0dc24
Marko Davidovic <davidovic-m@hotmail.com>
2020-08-23 00:14:54 -0400

    Final

---
+++ src/components/Wrongletters.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Wrongletters = ({wrongLetters}) => {
+  return (
+    <div className="wrong-letters-container">
+       <div>
+         {wrongLetters.length > 0 && 'Wrong'}
+         {wrongLetters
+           .map((letter, i) => <span key={i}>{letter}</span>)
+            .reduce((prev, curr) => prev === null ? [curr] : [prev, ', ', curr], null)}
+       </div>
+     </div>
+  )
+}
+
+export default  Wrongletters;
